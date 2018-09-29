e2152212a69c285dfff6730957f50591609b7888
Adrian Bolinger <adrian.bolinger@me.com>
2018-09-28 21:38:27 -0400

    Initial Commit

---
+++ StaticTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StaticTableView
+//
+//  Created by Adrian Bolinger on 9/28/18.
+//  Copyright © 2018 Adrian Bolinger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
